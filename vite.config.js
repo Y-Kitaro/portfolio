d1433641ec92b5f45e4a2553276494f4fbccae2a
Y-Kitaro <yhatakt101@gmail.com>
2025-06-15 00:56:03 +0900

    [update] ビルド時のファイル出力先をdocsに変更

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // 出力先を 'docs' に変更
+    outDir: 'docs', 
+  },
 })
